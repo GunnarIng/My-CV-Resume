2e8ba62b7f5d98ba60ed80d55feb38ccdf3212bf
GunnarIng <thomas.g.ingvarsson@gmail.com>
2022-11-24 13:36:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## My Webpage Project
+
+In this project I shall introduce myself and show my earlier projects in a portfolio. I´m making the page interactive with javaScript and to increase the Ux overall. 
+
+
+
+
+
---
+++ main.js
@@ -0,0 +1,22 @@
+window.addEventListener("DOMContentLoaded", main)
+
+function main() {
+  addEventListeners();
+}
+
+function addEventListeners() {
+  const menuButton = document.getElementById("menu-button");
+  menuButton.addEventListener('click', toggleMenu);
+}
+
+function toggleMenu () {
+  // Visa/dölj navbaren
+  const navbar = document.getElementById("navbar")
+  navbar.classList.toggle("open")
+
+  // Byt menyknappen till ett kryss
+  const menuIcon = document.querySelector("#menu-button i");
+  menuIcon.classList.toggle("fa-xmark");
+  menuIcon.classList.toggle("fa-bars");
+}
+
